861d90d16fcdee29e217ec6954a93e864cc206c8
rakesh <rakesh@fctrz.in>
2023-12-16 21:09:44 +0000

    logout button fix

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -41,26 +41,28 @@ function HomePage() {
         className="absolute z-0 object-cover w-full opacity-0 dark:opacity-100 sm:hidden"
         style={{ height: "200px" }}
       />
-      <div
-        onClick={handleLogout}
-        className="fixed flex justify-center gap-2 items-center right-2 top-2 text-zinc-300 cursor-pointer"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-6 h-6"
+      <button>
+        <div
+          onClick={handleLogout}
+          className="fixed flex justify-center gap-2 items-center right-2 top-2 text-zinc-300 cursor-pointer"
         >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M5.636 5.636a9 9 0 1012.728 0M12 3v9"
-          />
-        </svg>
-        <span>Logout</span>
-      </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M5.636 5.636a9 9 0 1012.728 0M12 3v9"
+            />
+          </svg>
+          <span>Logout</span>
+        </div>
+      </button>
 
       <Todo />
     </div>
